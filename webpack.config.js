545cc49dd3877266aea5066488fb07a078fea544
Obinna Ngini <obinnangini@gmail.com>
2017-12-07 18:08:30 -0500

    Updated config method

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const merge = require('webpack-merge');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -13,7 +12,13 @@ const configMethod = (minify) => {
   }
 
   const outputFile = `${outputFileBase}.css`;
-  const configObj = {
+
+  return {
+    entry: './scss/obi-bootstrap.scss',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'dummy.bundle.js',
+    },
     module: {
       rules: [
         {
@@ -34,46 +39,31 @@ const configMethod = (minify) => {
             ],
           }),
         },
+        {
+          test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'fonts/[name].[ext]',
+                publicPath: '../',
+              },
+            },
+          ],
+          exclude: /node_modules/,
+        },
       ],
     },
     plugins: [
+      new StyleLintPlugin(),
       new ExtractTextPlugin(outputFile),
+      new CopyWebpackPlugin([
+        { from: './scss', to: 'scss' },
+        { from: './node_modules/bootstrap/dist/js/bootstrap.js', to: 'js' },
+        { from: './package.json', to: 'package.json' },
+      ]),
     ],
   };
-  return configObj;
 };
 
-const commonConfig = {
-  entry: './scss/obi-bootstrap.scss',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'dummy.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]',
-              publicPath: '../',
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new StyleLintPlugin(),
-    new CopyWebpackPlugin([
-      { from: './scss', to: 'scss' },
-      { from: './node_modules/bootstrap/dist/js/bootstrap.js', to: 'js' },
-      { from: './package.json', to: 'package.json' },
-    ]),
-  ],
-};
-
-module.exports = [merge(commonConfig, configMethod()), merge(commonConfig, configMethod(true))];
+module.exports = [configMethod(), configMethod(true)];
